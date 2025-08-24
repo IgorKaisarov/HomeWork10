f4198f7244213f85004525478ab120b725fe7098
IgorKaisarov <Igo444@mail.ru>
2025-08-24 14:34:12 +0600

    Task 1, 2, 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.printf("Home Work 10!");
+
+        // Task 1
+        System.out.println(" Task 1 ");
+
+
+        String firstName = "Ivanov";
+        String secondName = "Ivan";
+        String lastName = "Ivanovich";
+        System.out.println("Ф.И.О. сотрудника - " + firstName + " " + secondName + " " + lastName);
+
+        // Task 2
+        System.out.println(" Task 2 ");
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+//        fullName = "IVANOV IVAN IVANOVICH";
+//        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
+
+        // Task 3
+        System.out.println(" Task 3 ");
+
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1);
+
+
     }
 }
